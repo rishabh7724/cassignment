39416c0a245661c19365a86d071b9316cc1cbb8a
rishabh7724 <46398462+rishabh7724@users.noreply.github.com>
2021-04-16 13:47:40 +0530

    Update Program07.bash

--- Program07.bash
+++ Program07.bash
@@ -9,3 +9,22 @@ do
     temp=`expr $temp / 10`
 done
 echo $n
+
+
+power () {
+    pow=1
+    for((i=1;i<=$m;i++))
+    do
+    pow=$(($pow*$n))
+    done
+    echo $pow
+    return $pow
+}
+
+echo "Enter number and power"
+read n
+read m
+
+power n m
+a=$?
+echo "Power ${a}"
